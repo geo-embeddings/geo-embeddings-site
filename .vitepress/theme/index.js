044abf87d34aad5ff0fd34b75ae487958d1e663c
Martha <martha@developmentseed.org>
2026-03-16 14:10:39 -0600

    clean up unused roadmap

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,10 +1,6 @@
 import DefaultTheme from "vitepress/theme";
-import RoadmapTracker from "./RoadmapTracker.vue";
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component("RoadmapTracker", RoadmapTracker);
-  },
 };
